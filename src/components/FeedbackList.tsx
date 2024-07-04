c4888719904be9cfcc8cfa6effc2f398811fddb9
Navneet Singh <116500700+Navneet2569@users.noreply.github.com>
2024-07-04 22:30:13 +0530

    FeedbackForm  and FeedbackList structured

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,5 +1,28 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
 function FeedbackList() {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>593</span>
+        </button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+          <p>Bytegrad</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea iusto
+            quasi animi. Tenetur, quas quidem.
+          </p>
+        </div>
+
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 }
 
 export default FeedbackList;
